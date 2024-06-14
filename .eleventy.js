ff914a8c40f06fab6c242c07dfd6999fe2bc62e4
PK <doubleobelisk@gmail.com>
2024-06-14 17:46:23 -0400

    fixed CNAME passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("./src/CNAME");
 
   return {
     dir: {
